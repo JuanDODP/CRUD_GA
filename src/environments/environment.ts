ecd75944760aa06ed284616897951f651ffbda20
Juan Dominguez <juan.dominguez.dp@gmail.com>
2026-04-14 11:36:07 -0600

    stable with api prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/api',
+  // produccion
+  baseUrl: 'https://crudga.duckdns.org/api',
+  // desarrollo
+  //  baseUrl: 'http://localhost:3000/api',
 
 };
